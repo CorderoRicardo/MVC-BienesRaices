0267e8326b5a78027fdffb47a40afb6b18fdd6a8
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-21 18:13:25 -0600

    Adjust path of files

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -12,38 +12,38 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Bienes Raíces</title>
-        <link rel="stylesheet" href="/S26-BienesRaices/build/css/app.css" />
+        <link rel="stylesheet" href="build/css/app.css" />
     </head>
     <body>
         <header class="header <?php echo $inicio ? 'inicio' :'' ?>">
             <div class="contenedor contenido-header">
                 <div class="barra">
-                    <a href="/S26-BienesRaices/index.php">
+                    <a href="index.php">
                         <img
-                            src="/S26-BienesRaices/build/img/logo.svg"
+                            src="build/img/logo.svg"
                             alt="logotipo de Bienes Raices"
                         />
                     </a>
 
                     <div class="mobile-menu">
                         <img
-                            src="/S26-BienesRaices/build/img/barras.svg"
+                            src="build/img/barras.svg"
                             alt="Icono menu responsive"
                         />
                     </div>
 
                     <div class="derecha">
                         <img
-                            src="/S26-BienesRaices/build/img/dark-mode.svg"
+                            src="build/img/dark-mode.svg"
                             class="dark-mode-boton"
                         />
                         <nav class="navegacion">
-                            <a href="/S26-BienesRaices/nosotros.php">Nosotros</a>
-                            <a href="/S26-BienesRaices/anuncios.php">Anuncios</a>
-                            <a href="/S26-BienesRaices/blog.php">Blog</a>
-                            <a href="/S26-BienesRaices/contacto.php">Contacto</a>
+                            <a href="nosotros.php">Nosotros</a>
+                            <a href="anuncios.php">Anuncios</a>
+                            <a href="blog.php">Blog</a>
+                            <a href="contacto.php">Contacto</a>
                             <?php if($auth): ?>
-                                <a href="/S26-BienesRaices/cerrar-sesion.php">Cerrar sesión</a>    
+                                <a href="cerrar-sesion.php">Cerrar sesión</a>    
                             <?php endif; ?>
                         </nav>
                     </div>
