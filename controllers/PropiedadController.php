2f22dce37e7c09fa700db5d9c9031ddd6cb94fd8
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-23 17:00:17 -0600

    Pass DB info from the model to the view in views/admin

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -2,11 +2,18 @@
 
 namespace Controllers;
 use MVC\Router;
+use Model\Propiedad;
 
 class PropiedadController{
     public static function index(Router $router){
+        // The model interacts with the DB and gets the data
+        $propiedades = Propiedad::all();
+        $resultado = null;
+
         $router->render('propiedades/admin',[
-            'mensaje' => "Esto es un placeholder"
+            // The data from the model is send to the view component
+            'propiedades' => $propiedades,
+            'resultado' => $resultado 
         ]);
     }
     public static function crear(){
