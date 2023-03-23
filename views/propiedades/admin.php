12ba9f09f64eb14f1562674849442cf60fcb09f1
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-23 17:01:44 -0600

    Render DB info onto the view

--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -1,3 +1,48 @@
-<h1>Admin</h1>
-<p>Desde la pagina admin</p>
-<p><?php echo $mensaje; ?></p>
+<main class="contenedor">
+    <h1>Administrador de Bienes Raices</h1>
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde-inline">Nueva propiedad</a>
+    <a href="/admin/vendedores/crear.php" class="boton boton-amarillo-inline">Nuevo vendedor</a>
+    <h2>Propiedades</h2>
+    <?php 
+        if($resultado){
+            $mensaje = mostrarNotificacion($resultado);
+            if($mensaje){
+    ?>
+                <p class="alerta exito">
+                    <?php echo cleanHTML($mensaje); ?>
+                </p>    
+    <?php
+            }
+        }
+    ?>
+
+    <table class="propiedades">
+            <thead>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </thead>
+
+            <tbody><!--Mostrar los resultados-->
+                <?php foreach($propiedades as $propiedad): ?>
+                <tr>
+                    <td> <?php echo $propiedad->id; ?></td>
+                    <td> <?php echo $propiedad->titulo ?></td>
+                    <td class=" tdImage"><img src="../imagenes/<?php echo $propiedad->imagen ?>" class="imagen-tabla"></td>
+                    <td> <?php echo '$' . $propiedad->precio ?> </td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                            <input type="hidden" name="tipo" value="propiedad">
+                            <input type="submit" class="boton-rojo" value="Eliminar">
+                        </form>
+                        <a href="propiedades/actualizar.php?id=<?php echo $propiedad->id ?>" class="boton-amarillo">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+
+    </table>
+</main>
