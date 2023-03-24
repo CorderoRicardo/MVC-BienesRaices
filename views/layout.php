72d4bfa80f952afbae8adfef0f3c3bdc3d83510e
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-23 18:16:41 -0600

    Adjust path for includes

--- views/layout.php
+++ views/layout.php
@@ -15,7 +15,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Bienes Raíces</title>
-        <link rel="stylesheet" href="build/css/app.css" />
+        <link rel="stylesheet" href="../build/css/app.css" />
     </head>
     <body>
         <header class="header <?php echo $inicio ? 'inicio' :'' ?>">
@@ -23,21 +23,21 @@
                 <div class="barra">
                     <a href="index.php">
                         <img
-                            src="build/img/logo.svg"
+                            src="../build/img/logo.svg"
                             alt="logotipo de Bienes Raices"
                         />
                     </a>
 
                     <div class="mobile-menu">
                         <img
-                            src="build/img/barras.svg"
+                            src="../build/img/barras.svg"
                             alt="Icono menu responsive"
                         />
                     </div>
 
                     <div class="derecha">
                         <img
-                            src="build/img/dark-mode.svg"
+                            src="../build/img/dark-mode.svg"
                             class="dark-mode-boton"
                         />
                         <nav class="navegacion">
@@ -72,6 +72,6 @@
             </div>
             <p class="copyright">Todos los derechos reservados <?php echo date('Y')?> &copy;</p>
         </footer>
-        <script src="build/js/bundle.min.js"></script>
+        <script src="../build/js/bundle.min.js"></script>
     </body>
 </html>
