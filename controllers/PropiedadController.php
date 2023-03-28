8b43297b9c3feddd232c1feccb830b79ff268324
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-28 13:13:58 -0600

    Implement controller method to delete properties

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -93,5 +93,20 @@ class PropiedadController{
             'vendedores' => $vendedores,
             'errores' => $errores
         ]);
-    }        
+    }     
+    
+    public static function eliminar(){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+
+            if($id){
+                $tipo = $_POST['tipo'];
+                if(validarTipoContenido($tipo)){
+                    $propiedad = Propiedad::find($id);
+                    $propiedad->eliminar();
+                }
+            }
+        }
+    }
 }
