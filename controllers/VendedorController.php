166d2bd41d671246c75f43118b16907f37604e4f
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-28 14:34:14 -0600

    Implement validation on POST request for 'crear' view

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -8,6 +8,19 @@ class VendedorController{
     public static function crear(Router $router){
         $errores = Vendedor::getErrores();
         $vendedor = new Vendedor;
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //Crear una instancia de vendedor
+            $vendedor = new Vendedor($_POST['vendedor']);
+
+            // Validar que no haya campos vacios
+            $errores = $vendedor->validar();
+
+            //no hay errores
+            if(empty($errores)){
+                $vendedor->guardar();
+            }            
+        }
         
         $router->render('vendedores/crear',[
             'errores' => $errores,
