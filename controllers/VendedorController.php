2452bf416ad5bb75b661fe0b617d117cf2b37535
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-29 12:53:13 -0600

    Implement 'eliminar' method for VendedorController

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -51,9 +51,20 @@ class VendedorController{
         ]);
     }
 
-    public static function eliminar(Router $router){
+    public static function eliminar(){
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
 
+            if($id){
+                $tipo = $_POST['tipo'];
+                if(validarTipoContenido($tipo)){
+                    if($tipo === "vendedor"){
+                        $vendedor = Vendedor::find($id);
+                        $vendedor->eliminar();
+                    }
+                }
+            }
         }
     }
     
