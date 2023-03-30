dd50982165c0c9da36a19d889110cab4e7142883
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-03-30 13:40:28 -0600

    Add controller for the blog CRUD

---
+++ controllers/BlogController.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Controllers;
+use MVC\Router;
+
+class BlogController{
+    public static function crear(){
+        echo "creando";
+    }
+
+    public static function actualizar(){
+        echo "actualizando";
+    }
+
+    public static function eliminar(){
+        echo "borrando";
+    }
+
+}
