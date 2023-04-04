f1140250c7d3099453d29e0c7834a426e1ae6edf
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-04-04 16:09:48 -0600

    Implement methods to validate user email and password

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -6,11 +6,34 @@ use Model\Admin;
 
 class LoginController{
     public static function login(Router $router){
+        $errores = Admin::getErrores();
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            echo "autenticación en progreso";
+            $auth = new Admin($_POST);
+            
+            $errores = $auth->validar();
+
+            if(empty($errores)){
+                //validar que el usuario existe
+                $resultado = $auth->existeUsuario();
+
+                if(!$resultado){
+                    $errores = Admin::getErrores();
+                }else{
+                    //verificar el password
+                    $autenticado = $auth->comprobarPassword($resultado);
+
+                    if($autenticado){
+                        //autenticar al usuario
+                        
+                    }else{
+                        $errores = Admin::getErrores();
+                    }
+                    
+                }
+            }
         }
         $router->render('auth/login',[
-            "errores" => []
+            "errores" => $errores
         ]);
     }
     public static function logout(){
