6c4731c0933baa0a35f44f43a438c1ae5bc999aa
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-04-04 12:08:32 -0600

    Add router paths for auth controller

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
 use Controllers\BlogController;
+use Controllers\LoginController;
 use Controllers\PaginasController;
 
 $router = new Router();
@@ -38,5 +39,11 @@ $router->get('/blog',[PaginasController::class,'blog']);
 $router->get('/entrada',[PaginasController::class,'entrada']);
 $router->get('/contacto',[PaginasController::class,'contacto']);
 $router->post('/contacto',[PaginasController::class,'contacto']);
+
+// Login and Auth
+$router->get('/login',[LoginController::class,'login']);
+$router->post('/login',[LoginController::class,'login']);
+$router->get('/logout',[LoginController::class,'logout']);
+
 
 $router->comprobarRutas();
