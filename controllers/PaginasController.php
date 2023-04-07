d4d4ddccfd6322458680bc93ff4d7d78f2e8cb0a
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-04-07 15:26:41 -0600

    Implement code to add text format on 'entrada' view

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Controllers;
 
@@ -8,49 +8,59 @@ use Model\Vendedor;
 use Model\Blog;
 use PHPMailer\PHPMailer\PHPMailer;
 
-class PaginasController{
-    public static function index(Router $router){
+class PaginasController
+{
+    public static function index(Router $router)
+    {
         $propiedades = Propiedad::getRows(3);
-        $entradas = Blog::getRows(2);        
-        $router->render('paginas/index',[
+        $entradas = Blog::getRows(2);
+        $router->render('paginas/index', [
             "propiedades" => $propiedades,
             "inicio" => true,
             "entradas" => $entradas
         ]);
     }
-    public static function nosotros(Router $router){
+    public static function nosotros(Router $router)
+    {
         $router->render('paginas/nosotros');
     }
-    public static function propiedades(Router $router){
-        $propiedades = Propiedad::getRows(10);        
-        $router->render('paginas/propiedades',[
-            'propiedades' =>$propiedades
-        ]);
-    }    
-    public static function propiedad(Router $router){
-        $id = validateIDfromURL('/propiedades');
-        $propiedad = Propiedad::find($id);
-        $router->render('paginas/propiedad',[
-            'propiedad' => $propiedad
-        ]);
-    }    
-    public static function blog(Router $router){
-        $entradas = Blog::getRows(9);
-        $router->render('paginas/blog',[
-            'entradas' => $entradas 
-        ]);
-    }    
-    public static function entrada(Router $router){
-        $id = validateIDfromURL('/blog');
-        $entrada = Blog::find($id);
-        $router->render('paginas/entrada',[
-            'entrada' => $entrada
+    public static function propiedades(Router $router)
+    {
+        $propiedades = Propiedad::getRows(10);
+        $router->render('paginas/propiedades', [
+            'propiedades' => $propiedades
         ]);
     }
-    public static function contacto(Router $router){
+    public static function propiedad(Router $router)
+    {
+        $id = validateIDfromURL('/propiedades');
+        $propiedad = Propiedad::find($id);
+        $router->render('paginas/propiedad', [
+            'propiedad' => $propiedad
+        ]);
+    }
+    public static function blog(Router $router)
+    {
+        $entradas = Blog::getRows(9);
+        $router->render('paginas/blog', [
+            'entradas' => $entradas
+        ]);
+    }
+    public static function entrada(Router $router)
+    {
+        $id = validateIDfromURL('/blog');
+        $entrada = Blog::find($id);
+        $parrafos = explode("\r", $entrada->contenido);
+        $router->render('paginas/entrada', [
+            'entrada' => $entrada,
+            'parrafos' => $parrafos
+        ]);
+    }
+    public static function contacto(Router $router)
+    {
         $mensaje = null;
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $respuestas = $_POST['contacto'];
             // nueva instancia de phpmailer
             $mail = new PHPMailer();
@@ -60,8 +70,8 @@ class PaginasController{
             $mail->SMTPAuth = true;
             $mail->Port = 2525;
             $mail->Username = '73b3da268e5fd9';
-            $mail->Password = '213750ad594f2e';           
-            $mail->SMTPSecure = 'tls'; 
+            $mail->Password = '213750ad594f2e';
+            $mail->SMTPSecure = 'tls';
             //configurar el contenido del mail
             $mail->setFrom('admin@bienesraices.com');
             $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
@@ -73,31 +83,31 @@ class PaginasController{
             $contenido = "<html>";
             $contenido .= "<p> Tienes un nuevo mensaje</p>";
             $contenido .= "<ul>";
-            $contenido .= "<li>Nombre: ".$respuestas['nombre']."</li>";
-            $contenido .= "<li>Mensaje: ".$respuestas['mensaje']."</li>";
-            $contenido .= "<li>Vende o compra: ".$respuestas['tipo']."</li>";
-            $contenido .= "<li>Precio o presupuesto: $".number_format($respuestas['precio'])."</li>";
-            $contenido .= "<li>Forma de contacto: ".$respuestas['contacto']."</li>";
-            if($respuestas['contacto'] === 'telefono'){
-                $contenido .= "<li>Teléfono: ".$respuestas['telefono']."</li>";                
-                $contenido .= "<li>Fecha: ".$respuestas['fecha']."</li>";
-                $contenido .= "<li>Hora: ".$respuestas['hora']."</li>";
-            }else{
-                $contenido .= "<li>Email: ".$respuestas['email']."</li>";
+            $contenido .= "<li>Nombre: " . $respuestas['nombre'] . "</li>";
+            $contenido .= "<li>Mensaje: " . $respuestas['mensaje'] . "</li>";
+            $contenido .= "<li>Vende o compra: " . $respuestas['tipo'] . "</li>";
+            $contenido .= "<li>Precio o presupuesto: $" . number_format($respuestas['precio']) . "</li>";
+            $contenido .= "<li>Forma de contacto: " . $respuestas['contacto'] . "</li>";
+            if ($respuestas['contacto'] === 'telefono') {
+                $contenido .= "<li>Teléfono: " . $respuestas['telefono'] . "</li>";
+                $contenido .= "<li>Fecha: " . $respuestas['fecha'] . "</li>";
+                $contenido .= "<li>Hora: " . $respuestas['hora'] . "</li>";
+            } else {
+                $contenido .= "<li>Email: " . $respuestas['email'] . "</li>";
             }
             $contenido .= "</ul>";
             $contenido .= "</html";
             $mail->Body = $contenido;
             $mail->AltBody = "Texto alternativo/plano";
             //enviar el email
-            if($mail->send()){
+            if ($mail->send()) {
                 $mensaje = "Mensaje enviado correctamente";
-            }else{
+            } else {
                 $mensaje = "El mensaje no se pudo enviar...";
             }
         }
-        $router->render('paginas/contacto',[
+        $router->render('paginas/contacto', [
             'mensaje' => $mensaje
         ]);
-    }        
+    }
 }
--- views/paginas/entrada.php
+++ views/paginas/entrada.php
@@ -3,18 +3,19 @@
     <picture>
         <!-- <source srcset="build/img/destacada2.webp" type="image/webp" />
         <source srcset="build/img/destacada2.jpg" type="image/jpeg" /> -->
-        <img
-            loading="lazy"
-            src="../imagenes/entradas/<?php echo $entrada->imagen; ?>"
-            alt="Imagen entrada blog"
-        />
+        <img loading="lazy" src="../imagenes/entradas/<?php echo $entrada->imagen; ?>" alt="Imagen entrada blog" />
     </picture>
     <div class="resumen-propiedad">
         <p class="informacion-meta">
-            Escrito el <span><?php echo date_format(date_create($entrada->creado),"d/m/Y"); ?></span> por <span><?php echo $entrada->autor; ?></span>
+            Escrito el <span><?php echo date_format(date_create($entrada->creado), "d/m/Y"); ?></span> por <span><?php echo $entrada->autor; ?></span>
         </p>
-        <p>
-            <?php echo $entrada->contenido; ?>
-        </p>
+        <?php
+        foreach ($parrafos as $parrafo) :
+            if (strlen($parrafo) > 1) :
+        ?>
+                <p><?php echo $parrafo; ?></p>
+        <?php
+            endif;
+        endforeach; ?>
     </div>
 </main>
